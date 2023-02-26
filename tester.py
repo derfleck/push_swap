df5bf5bbf6239beb61744c91fcbf263608bb2af2
Michael Leitner <abraxas.bladeking@gmail.com>
2023-02-26 17:28:49 +0100

    optimized tester, included checker

--- tester.py
+++ tester.py
@@ -1,58 +1,74 @@
 import random
 import subprocess
 import statistics
+import sys
 
-# Define the number of tests to run for each number of arguments
-num_runs = 500
 
-# Define the number of arguments to test
-num_tests = [3, 5, 100, 500]
+def main(argv):
+    # Define the number of tests to run for each number of arguments
+    if int(argv[1]) > 0:
+        num_runs = int(argv[1])
+    else:
+        num_runs = 100
 
-# Define the range of unsigned integer values to use as arguments
-min_val = -2147483648
-max_val = 2147483647
+    # Define the number of arguments to test
+    num_tests = [3, 5, 100, 500]
 
-# Initialize empty lists to store the number of lines for each test
-num_lines_3 = []
-num_lines_5 = []
-num_lines_100 = []
-num_lines_500 = []
+    # Define the range of unsigned integer values to use as arguments
+    min_val = -2147483648
+    max_val = 2147483647
 
-# Loop through the different number of tests to run
-for num in num_tests:
-    # Run multiple tests for each number of arguments
-    for i in range(num_runs):
-        # Generate a list of unique random unsigned integers within the specified range
-        args = random.sample(range(min_val, max_val), num)
-        print(args)
-        # Convert the arguments list to a string with spaces between each value
-        args_str = [str(arg) for arg in args]
-        # Call the push_swap executable with the generated arguments
-        result = subprocess.run(["./push_swap"] + args_str, stdout=subprocess.PIPE)
-        # Get the number of lines returned by push_swap
-        num_lines = result.stdout.count(b'\n')
-        # Append the number of lines to the appropriate list based on the number of arguments
-        if num == 3:
-            num_lines_3.append(num_lines)
-        elif num == 5:
-            num_lines_5.append(num_lines)
-        elif num == 100:
-            num_lines_100.append(num_lines)
-        elif num == 500:
-            num_lines_500.append(num_lines)
+    # Initialize empty lists to store the number of lines for each test
+    num_lines_3 = []
+    num_lines_5 = []
+    num_lines_100 = []
+    num_lines_500 = []
+    errors = 0
 
-# Calculate the average number of lines per argument for each number of arguments tested
-avg_num_lines_3 = statistics.mean(num_lines_3)
-max_num_lines_3 = max(num_lines_3)
-avg_num_lines_5 = statistics.mean(num_lines_5)
-max_num_lines_5 = max(num_lines_5)
-avg_num_lines_100 = statistics.mean(num_lines_100)
-max_num_lines_100 = max(num_lines_100)
-avg_num_lines_500 = statistics.mean(num_lines_500)
-max_num_lines_500 = max(num_lines_500)
+    # Loop through the different number of tests to run
+    for num in num_tests:
+        # Run multiple tests for each number of arguments
+        for i in range(num_runs):
+            # Generate a list of unique random unsigned integers within the specified range
+            args = random.sample(range(min_val, max_val), num)
+            # Convert the arguments list to a string with spaces between each value
+            args_str = [str(arg) for arg in args]
+            # Call the push_swap executable with the generated arguments
+            result = subprocess.run(["./push_swap"] + args_str, stdout=subprocess.PIPE)
+            # Get the number of lines returned by push_swap
+            num_lines = result.stdout.count(b'\n')
+            #Use the output of result and pass it to the checker executable
+            checker = subprocess.run(["./checker"] + args_str, input=result.stdout, stdout=subprocess.PIPE)
+            if checker.stdout != b'OK\n':
+                print("Error: " + args_str)
+                errors += 1
+            # Append the number of lines to the appropriate list based on the number of arguments
+            if num == 3:
+                num_lines_3.append(num_lines)
+            elif num == 5:
+                num_lines_5.append(num_lines)
+            elif num == 100:
+                num_lines_100.append(num_lines)
+            elif num == 500:
+                num_lines_500.append(num_lines)
 
-# Print the results
-print(f"Average for 3 arguments: {avg_num_lines_3} Maximum: {max_num_lines_3}")
-print(f"Average for 5 arguments: {avg_num_lines_5} Maximum: {max_num_lines_5}")
-print(f"Average for 100 arguments: {avg_num_lines_100} Maximum: {max_num_lines_100}")
-print(f"Average for 500 arguments: {avg_num_lines_500} Maximum: {max_num_lines_500}")
+    # Calculate the average number of lines per argument for each number of arguments tested
+    avg_num_lines_3 = statistics.mean(num_lines_3)
+    max_num_lines_3 = max(num_lines_3)
+    avg_num_lines_5 = statistics.mean(num_lines_5)
+    max_num_lines_5 = max(num_lines_5)
+    avg_num_lines_100 = statistics.mean(num_lines_100)
+    max_num_lines_100 = max(num_lines_100)
+    avg_num_lines_500 = statistics.mean(num_lines_500)
+    max_num_lines_500 = max(num_lines_500)
+
+    # Print the results
+    print(f"Number of runs: {num_runs}")
+    print(f"Errors: {errors}")
+    print(f"Average for 3 arguments: {avg_num_lines_3} Maximum: {max_num_lines_3}")
+    print(f"Average for 5 arguments: {avg_num_lines_5} Maximum: {max_num_lines_5}")
+    print(f"Average for 100 arguments: {avg_num_lines_100} Maximum: {max_num_lines_100}")
+    print(f"Average for 500 arguments: {avg_num_lines_500} Maximum: {max_num_lines_500}")
+
+if __name__ == "__main__":
+    main(sys.argv)
